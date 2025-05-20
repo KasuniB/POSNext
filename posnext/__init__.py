59af0de77e74dc21dc92b88acd68ccd6f293ce1e
KasuniB <113013082+KasuniB@users.noreply.github.com>
2025-05-20 13:11:35 +0300

    Update __init__.py

--- posnext/__init__.py
+++ posnext/__init__.py
@@ -1,1 +1,8 @@
 __version__ = "0.0.1"
+import sys
+import os
+
+# Add inner folder to path if not already present
+inner_path = os.path.join(os.path.dirname(__file__), "posnext")
+if inner_path not in sys.path:
+    sys.path.insert(0, inner_path)
