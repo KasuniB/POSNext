bc53ccc20b0f3bf54f945a1c30a325283cbf8294
KasuniB <113013082+KasuniB@users.noreply.github.com>
2025-05-20 12:07:03 +0300

    Update item_daily_tracker.js

--- posnext/posnext/doctype/item_daily_tracker/item_daily_tracker.js
+++ posnext/posnext/doctype/item_daily_tracker/item_daily_tracker.js
@@ -3,22 +3,23 @@
 
 frappe.ui.form.on('Item Daily Tracker', {
     refresh: function(frm) {
-        // Add custom buttons
+        // Add custom button to fetch data
         frm.add_custom_button(__('Fetch Data'), function() {
+            // Trigger save to call validate and fetch_reconciliation_data
             frm.save();
         });
         
-        // Highlight differences in the table
+        // Highlight differences in the items table
         frm.fields_dict['items'].grid.wrapper.find('.grid-row').each(function(i, item) {
-            var difference = frm.doc.items[i].difference;
-            if (difference !== 0) {
-                $(item).find('.grid-row-check').parent().css('background-color', '#fff9f9');
+            var row = frm.doc.items[i];
+            if (row && row.difference !== 0) {
+                $(item).css('background-color', '#fff9f9');
             }
         });
     },
     
     pos_opening_entry: function(frm) {
-        // If POS Opening Entry is set, get related company
+        // When POS Opening Entry is set, fetch company and date
         if (frm.doc.pos_opening_entry) {
             frappe.call({
                 method: 'frappe.client.get_value',
@@ -34,9 +35,16 @@ frappe.ui.form.on('Item Daily Tracker', {
                     if (data) {
                         frm.set_value('company', data.company);
                         frm.set_value('date', data.period_start_date);
+                        // Automatically trigger data fetch
+                        frm.save();
                     }
                 }
             });
+        } else {
+            // Clear fields if pos_opening_entry is unset
+            frm.set_value('company', '');
+            frm.set_value('date', '');
+            frm.set_value('items', []);
         }
     }
 });
